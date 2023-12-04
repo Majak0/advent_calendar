f57ff6efe32d04dece75cc91b648fd66dea2ec9e
Maxime <maxime@cruiseline.eu>
2023-12-04 09:26:18 +0100

    Complete part 2 day 1

---
+++ test.py
@@ -0,0 +1,39 @@
+import re
+# ====================== day 1 : part 2 ====================== #
+
+words = {
+    'zero' : '0',
+    'one' : '1',
+    'two' : '2',
+    'three' : '3',
+    'four' : '4',
+    'five' : '5',
+    'six' : '6',
+    'seven' : '7',
+    'eight' : '8',
+    'nine' : '9'
+}
+
+file = open('inputs/day1.txt', "r")
+line = file.readline()
+sum = 0
+while line:
+    index = 0
+    while index < len(line):
+        for word in words :
+            if line[index:index+len(word)]==word:
+                line = line.replace(word, words[word],1)
+            if index+len(word) >= len(line)-index :
+                if line[index:]==word or line[index:]==word+"\n":
+                    line = line.replace(word, words[word],1)
+                
+
+        index+=1
+    print(line)
+    line = ''.join(re.findall(r'\d+', line)) # Delete non numeric chars
+    add = line[0] + line[-1] # Create number from first and last digit of string
+    print(line," - ",line[0]," - ",line[-1]," : ",add,"\n------------------------\n")
+    sum += int(add)
+    line = file.readline() # Next line
+file.close()
+print("=============",sum,"=============")
