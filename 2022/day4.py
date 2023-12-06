8b675baf9cd836d39e39ee8a47b5be87e018101a
Maxime <maxime@cruiseline.eu>
2023-12-06 16:16:02 +0100

    remove non necessary prints

--- 2022/day4.py
+++ 2022/day4.py
@@ -30,7 +30,6 @@ line = file.readline()
 pairs = 0
 while line:
     line = line.replace("\n","")
-    print(line,':',pairs)
     datas = line.split(',')
     firstElf = datas[0].split('-')
     secondElf = datas[1].split('-')
@@ -39,13 +38,12 @@ while line:
     list2 = []
     for num in range(int(firstElf[0]),int(firstElf[-1])+1):
         list1.append(num)
-    print(list1)
+
     for num in range(int(secondElf[0]),int(secondElf[-1])+1):
         list2.append(num)
-    print(list2,"\n==================\n")
+
     if any(item in list1 for item in list2) or any(item in list2 for item in list1):
         pairs+=1
-        print(pairs)
-    line = file.readline() # Next line
+    line = file.readline()
 file.close()
 print("=============",pairs,"=============")
