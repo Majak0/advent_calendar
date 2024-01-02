f6d2352715c9e43186ed9f3c3df8448f3c8bcbb4
Maxime <maxime@cruiseline.eu>
2024-01-02 10:08:22 +0100

    save

--- other/igemoe.php
+++ other/igemoe.php
@@ -1,6 +1,12 @@
 <?php
-
+$log = "Ouverture fichier entrée\n";
 $file = fopen("datas/in", "r");
+
+$sql = "SELECT lang FROM croisiere2018.lang_translation group by lang;";
+//$langs = dataBaseGetAssoc($sql);
+$log .= "\nLancement requête SQL\n";
+
+$log .= "\nRécupérations ID sans carte image IGEMOE\n";
 if ($file) {
     $lang = 'fr'; // Valeure de test
     $itinerairesID = [];
@@ -14,9 +20,21 @@ if ($file) {
     fclose($file);
 }
 
-$output = [];
-foreach($itinerairesID as $id => $filename){
-    if (!file_exists($filename)) $output[] = $id;
+$log .= "\nSuppression contenu fichier sortie\n";
+$path = "datas/out";
+$fileOut = fopen($path, "r+");
+if ($fileOut !== false) {
+    file_put_contents($path,null);
 }
 
-print_r($output);
+$log .= "\nEcriture dans fichier sortie\n";
+foreach($itinerairesID as $id => $filename){
+    if (!file_exists($filename)) file_put_contents($path,$id."\n", FILE_APPEND);
+}
+fclose($fileOut);
+$log .= "\nFichier de sortie fermé\n";
+// foreach($output as $key => $id){
+//    UPDATE carte SET img=0 WHERE id_cruise=:id;
+//    :id = $id;
+//}
+echo $log;
