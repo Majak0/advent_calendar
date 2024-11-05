39f5ff458e46e6f74cc7da93d14313d775382694
Maxime <maxime@cruiseline.eu>
2024-11-05 10:22:47 +0100

    update

--- 2015/NotQuiteLisp.java
+++ 2015/NotQuiteLisp.java
@@ -3,6 +3,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 class NotQuiteLisp {
     public static void main(String[] args) {
+        System.out.println(partOne());
+        System.out.println(partTwo());
+    }
+
+    private static int partOne(){
         int result = 0;
         try {
             String line = Files.readString(Paths.get("2015/inputs/day1.txt"));
@@ -12,6 +17,24 @@ class NotQuiteLisp {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(result);
+        return result;
+    }
+
+    private static int partTwo(){
+        int result = 0;
+        int index = 0;
+        try {
+            String line = Files.readString(Paths.get("2015/inputs/day1.txt"));
+            for (char caractere : line.toCharArray()) {
+                result = caractere == '(' ? result+1 : result-1;
+                index++;
+                if (result == -1) {
+                    return(index);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
